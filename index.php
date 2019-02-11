9a915fed35dfe8157ee2f3ab7f299e59187ee7f2
Jairo <jairobernaldev@gmail.com>
2019-02-11 20:23:17 +0100

    Estilos de los post.

--- index.php
+++ index.php
@@ -79,35 +79,48 @@
         <div id="principal">
             <h1>Ultimas entradas</h1>
             <article class="entrada">
-                <h2>Titulo de mi entrada</h2>
-                <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac magna fringilla, lobortis quam sit amet, pretium ex. Vivamus in maximus justo. Fusce bibendum ut nibh ut tempor. Nunc et pharetra neque. Donec vitae suscipit nisi. Aenean in aliquam dui. Mauris quis risus ut eros rhoncus placerat. Mauris sollicitudin bibendum mauris. Phasellus quis ligula sed turpis sagittis accumsan. Donec vitae elit accumsan, euismod ligula et, sodales risus. Nulla facilisi. Sed ligula mauris, fermentum ut iaculis vitae, suscipit id sapien.
-                </p>
+                <a href="">
+                    <h2>Titulo de mi entrada</h2>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac magna fringilla, lobortis quam sit amet, pretium ex. Vivamus in maximus justo. Fusce bibendum ut nibh ut tempor. Nunc et pharetra neque. Donec vitae suscipit nisi. Aenean in aliquam dui. Mauris quis risus ut eros rhoncus placerat. Mauris sollicitudin bibendum mauris. Phasellus quis ligula sed turpis sagittis accumsan. Donec vitae elit accumsan, euismod ligula et, sodales risus. Nulla facilisi. Sed ligula mauris, fermentum ut iaculis vitae, suscipit id sapien.
+                    </p>
+                </a>
             </article>
 
             <article class="entrada">
-                <h2>Titulo de mi entrada</h2>
-                <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac magna fringilla, lobortis quam sit amet, pretium ex. Vivamus in maximus justo. Fusce bibendum ut nibh ut tempor. Nunc et pharetra neque. Donec vitae suscipit nisi. Aenean in aliquam dui. Mauris quis risus ut eros rhoncus placerat. Mauris sollicitudin bibendum mauris. Phasellus quis ligula sed turpis sagittis accumsan. Donec vitae elit accumsan, euismod ligula et, sodales risus. Nulla facilisi. Sed ligula mauris, fermentum ut iaculis vitae, suscipit id sapien.
-                </p>
+                <a href="">
+                    <h2>Titulo de mi entrada</h2>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac magna fringilla, lobortis quam sit amet, pretium ex. Vivamus in maximus justo. Fusce bibendum ut nibh ut tempor. Nunc et pharetra neque. Donec vitae suscipit nisi. Aenean in aliquam dui. Mauris quis risus ut eros rhoncus placerat. Mauris sollicitudin bibendum mauris. Phasellus quis ligula sed turpis sagittis accumsan. Donec vitae elit accumsan, euismod ligula et, sodales risus. Nulla facilisi. Sed ligula mauris, fermentum ut iaculis vitae, suscipit id sapien.
+                    </p>
+                </a>
             </article>
 
             <article class="entrada">
-                <h2>Titulo de mi entrada</h2>
-                <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac magna fringilla, lobortis quam sit amet, pretium ex. Vivamus in maximus justo. Fusce bibendum ut nibh ut tempor. Nunc et pharetra neque. Donec vitae suscipit nisi. Aenean in aliquam dui. Mauris quis risus ut eros rhoncus placerat. Mauris sollicitudin bibendum mauris. Phasellus quis ligula sed turpis sagittis accumsan. Donec vitae elit accumsan, euismod ligula et, sodales risus. Nulla facilisi. Sed ligula mauris, fermentum ut iaculis vitae, suscipit id sapien.
-                </p>
+                <a href="">
+                    <h2>Titulo de mi entrada</h2>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac magna fringilla, lobortis quam sit amet, pretium ex. Vivamus in maximus justo. Fusce bibendum ut nibh ut tempor. Nunc et pharetra neque. Donec vitae suscipit nisi. Aenean in aliquam dui. Mauris quis risus ut eros rhoncus placerat. Mauris sollicitudin bibendum mauris. Phasellus quis ligula sed turpis sagittis accumsan. Donec vitae elit accumsan, euismod ligula et, sodales risus. Nulla facilisi. Sed ligula mauris, fermentum ut iaculis vitae, suscipit id sapien.
+                    </p>
+                </a>
             </article>
 
             <article class="entrada">
-                <h2>Titulo de mi entrada</h2>
-                <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac magna fringilla, lobortis quam sit amet, pretium ex. Vivamus in maximus justo. Fusce bibendum ut nibh ut tempor. Nunc et pharetra neque. Donec vitae suscipit nisi. Aenean in aliquam dui. Mauris quis risus ut eros rhoncus placerat. Mauris sollicitudin bibendum mauris. Phasellus quis ligula sed turpis sagittis accumsan. Donec vitae elit accumsan, euismod ligula et, sodales risus. Nulla facilisi. Sed ligula mauris, fermentum ut iaculis vitae, suscipit id sapien.
-                </p>
+                <a href="">
+                    <h2>Titulo de mi entrada</h2>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac magna fringilla, lobortis quam sit amet, pretium ex. Vivamus in maximus justo. Fusce bibendum ut nibh ut tempor. Nunc et pharetra neque. Donec vitae suscipit nisi. Aenean in aliquam dui. Mauris quis risus ut eros rhoncus placerat. Mauris sollicitudin bibendum mauris. Phasellus quis ligula sed turpis sagittis accumsan. Donec vitae elit accumsan, euismod ligula et, sodales risus. Nulla facilisi. Sed ligula mauris, fermentum ut iaculis vitae, suscipit id sapien.
+                    </p>
+                </a>
             </article>
+            <div id="ver-todas">
+                <a href="">Ver todas las entradas</a>
+            </div>
         </div>
+        
     </div>
-
+    </div>
+    <div class="clearfix"></div>
     <footer id="pie">
         <p>
             Desarrollado por Jairo Bernal &copy; 2019
